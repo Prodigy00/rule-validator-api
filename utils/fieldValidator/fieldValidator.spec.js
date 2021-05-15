346849fef92a2c6113ebd357963f6a57e7c45071
Prodigy00 <simplygiddy@gmail.com>
2021-05-15 16:53:47 +0100

    added all tests for isValidJSONObject fnc

--- utils/fieldValidator/fieldValidator.spec.js
+++ utils/fieldValidator/fieldValidator.spec.js
@@ -11,14 +11,14 @@ const inputWithoutRule = {
 };
 
 describe('Test field validator module', () => {
-  describe('test rule checker fnc', () => {
-    test('return an error message if rule field is not present', () => {
+  describe('test fnc for validating rule field', () => {
+    it('returns an error message if rule field is not present', () => {
       const message = 'rule is required';
       expect(fieldValidator.ruleCheck(inputWithoutRule)).toMatch(message);
     });
 
-    test('rule field should be valid JSON object', () => {});
+    it('should be valid JSON object', () => {});
 
-    test('return obj if rule field exists and is valid', () => {});
+    it('return obj if rule field exists and is valid', () => {});
   });
 });
